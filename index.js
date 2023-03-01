ea42b81367066c04d07e466ef702bc86624e8f6e
MuntasirAbid <muntasira7@hotmail.com>
2023-03-01 23:57:16 +0100

    connected mongo

--- index.js
+++ index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 require("dotenv").config()
 
 const app = express()
 
-app.use("*", (req, res, next) => {
-    console.log(`Request received: ${req.method} ${req.url}`);
-    next();
-});
+/**
+ * 
+ * API
+ * route
+ * /users (paginated)
+ * /user/create
+ * /login
+ * /register
+ * /users (protected)
+ *  
+ * 
+ * 
+ */
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tkhdgb3.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
 
 app.get('/test', (req, res) => {
     res.json({
@@ -15,5 +34,17 @@ app.get('/test', (req, res) => {
 })
 
 app.listen(process.env.PORT || 10000, () => {
+
+    client.connect((err) => {
+
+        if (err) {
+            console.log(err);
+        }
+        else {
+
+            console.log("Connected to mongoDB");
+            client.close()
+        }
+    });
     console.log("server is running on " + process.env.PORT);
 })
